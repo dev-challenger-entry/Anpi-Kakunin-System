8adecfd0a92a2658e6ba15eb40a292965e4dc4a1
dev-challenger <294498228+dev-challenger-entry@users.noreply.github.com>
2026-08-15 15:11:53 +0900

    feat:reCAPTCHA認証後の動線を確立
    管理画面への動線を確立した。セキュリティチェックはまたのちほどの課題にする。今回は動線を確立できたというコミット。

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -19,6 +19,8 @@ function App() {
   // 例：'ADMIN'、'USER'
   const [role, setRole] = useState(null)
 
+  //管理者が、確認画面(CAPTCHA）を突破したかどうか
+  const [captchaVerified, setCaptchaVerified] = useState(false)
 
   // Loginコンポーネントから呼び出される
   // ログイン成功時に社員IDと権限を受け取る
@@ -27,11 +29,13 @@ function App() {
     setRole(role)
   }
 
+   //ログイン後に認証状態を保持
+  const handleCaptchaSuccess = () => {
+    setCaptchaVerified(true)
+  }
 
   return (
     <>
-
-      {/* システム共通ヘッダー */}
       <div className="system-header">
         <h1 className="system-title">
           安否確認システム
@@ -46,22 +50,23 @@ function App() {
       {/* ログイン状態・権限によって表示する画面を切り替える */}
       <div>
 
-        {/* まだログインしていない場合 → ログイン画面 */}
-        {!loggedInEmployeeId ? (
+        {/* まだログインしていない場合 → ログイン画面 */} 
 
+         {!loggedInEmployeeId ? (
           <Login onLoginSuccess={handleLoginSuccess} />
-
-        ) : role === 'ADMIN' ? (
-
-          /* ADMINの場合 → 管理者画面 */
-          <Recaptcha />
-
-        ) : (
-
-          /* ADMIN以外の場合 → マイページ */
+          /* ログイン済み ＋ ADMINの場合 → CAPTCHA認証を確認 */
+          ) : role === 'ADMIN' ? (
+          captchaVerified ? (
+          /* CAPTCHA認証済み → 管理者画面 */
+          <AdminStatusSummary />
+          ) : (
+           /* CAPTCHA未認証 → CAPTCHA画面 */
+          <Recaptcha onSuccess={handleCaptchaSuccess} />
+          )
+          ) : (
+            /* ADMINではない場合 → マイページ */
           <MyPage employeeId={loggedInEmployeeId} />
-
-        )}
+          )}
 
       </div>
 
