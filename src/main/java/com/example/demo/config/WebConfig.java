dab2bcac67fdbd4c80975c50b4ce644c0f41eb4f
dev-challenger <294498228+dev-challenger-entry@users.noreply.github.com>
2026-08-17 01:27:43 +0900

    Create WebConfig.java
    ブラウザ（フロント）でURLを直叩きするサーバー（Spring Boot）がリクエストを受け取るWebConfig（この設定）に基づき、Springが「インターセプターが必要か」を判断するSessionAuthInterceptor が実行され、認証をチェックするチェックOKなら、実際のコントローラー（APIの本体）へ処理が渡るという仕組みを目指したい。
    
    だから、認可チェックJavaの次に、認可すべきか？を判断するJavaを作った。
    
    ※フロントエンドから fetch('/api/mypage') などでバックエンドのAPIが呼び出された瞬間に、Spring Bootが「このURLはインターセプター（認証チェック）を通すべきか？」を判断するJavaを新規作成したコミット。
    これによって、
    
    GET  /api/mypage
    GET  /api/status/社員ID
    PUT  /api/status/社員ID
    
    に対してInterceptorが働きます。
    調べていると、一般社員と管理者で認可チェックは分けたほうがいいかもしれないと思った。
    そのため、今回は一般社員対象のみの認可チェックで一区切りつける。

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionAuthInterceptor())
+                .addPathPatterns(
+                    "/api/mypage",
+                    "/api/status/**"
+                )
+                .excludePathPatterns(
+                    "/api/login"
+                );
+    }
+}
