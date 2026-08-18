8a88608c827c920b6ac025cf8517617d32dacf46
dev-challenger <294498228+dev-challenger-entry@users.noreply.github.com>
2026-08-18 16:15:22 +0900

    feat: 社員情報の登録・変更画面の新規作成
    appearance: none;で、ブラウザ標準デザインを消せることを知った。次からはできればこれを活用できるようになりたい。
    既に管理者情報更新画面を作っていたのでまとめてコミット。
    起動テストは特に問題なし。
    次は、管理者集計結果画面の調整をしていく。

--- my-react-app/src/component/EmployeeManage.jsx
+++ my-react-app/src/component/EmployeeManage.jsx
@@ -1,9 +1,111 @@
-function EmployeeManage({ onBack }) {
+import './EmployeeManage.css'
+
+function EmployeeManage({ onBack, onLogout }) {
   return (
-    <div className="admin-card">
-      <h2 className="admin-title">社員情報登録・更新</h2>
-      <p>（ここに検索・登録・変更の機能を今後実装）</p>
-      <button onClick={onBack}>管理者画面に戻る</button>
+    <div className="employee-manage-container">
+
+      {/* 管理者画面の見出し */}
+      <div className="employee-manage-header">
+        <div>管理者画面</div>
+      </div>
+
+      {/* 画面タイトル */}
+      <h2 className="employee-manage-title">
+        社員情報登録・更新
+      </h2>
+
+      {/* 社員情報 */}
+      <div className="employee-manage-form">
+
+        {/* 社員ID */}
+        <label className="employee-manage-label">
+          社員ID
+        </label>
+
+        <input
+          type="text"
+          className="employee-manage-input"
+          placeholder="設定したいIDを入力"
+        />
+
+        {/* 社員名 */}
+        <label className="employee-manage-label">
+          社員名
+        </label>
+
+        <input
+          type="text"
+          className="employee-manage-input"
+          defaultValue="社員子"
+        />
+
+        {/* 部署名 */}
+        <label className="employee-manage-label">
+          部署名
+        </label>
+
+        <input
+          type="text"
+          className="employee-manage-input"
+          defaultValue="はなまる部署"
+        />
+
+        {/* 現在のパスワード */}
+        <label className="employee-manage-label">
+          現在のパスワード
+        </label>
+
+        <input
+          type="password"
+          className="employee-manage-input"
+        />
+
+        {/* 新しいパスワード */}
+        <label className="employee-manage-label">
+          新しいパスワード
+        </label>
+
+        <input
+          type="password"
+          className="employee-manage-input"
+        />
+
+        {/* 変更する */}
+        <button
+          type="button"
+          className="employee-manage-change-button"
+        >
+          変更する
+        </button>
+
+        {/* 削除する */}
+        <button
+          type="button"
+          className="employee-manage-delete-button"
+        >
+          削除する
+        </button>
+
+        {/* キャンセル */}
+        <button
+          type="button"
+          className="employee-manage-cancel-button"
+          onClick={onBack}
+        >
+          キャンセル
+        </button>
+
+        {/* ログアウト */}
+        <button
+          type="button"
+          className="employee-manage-logout-button"
+          onClick={onLogout}
+        >
+          ここからログアウトする
+        </button>
+
+      </div>
+
     </div>
   )
 }
