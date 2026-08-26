b5f921caaf137a3a9d53693fbb4ed9495525024c
dev-challenger <294498228+dev-challenger-entry@users.noreply.github.com>
2026-08-27 08:16:35 +0900

    dev: スマホでの起動テストのためにフロントエンド設定操作
    このコードをいれると同じWi-Fiアクセスポイントを使っている人は、同じローカルホストを共有する仕組みが導入される。
    今まで、パソコンでループバックしていたが、これでスマホテストが可能になった。
    しかし、わたしはセキュリティ的に許せなかったので、パソコンのファイアウォールのほうに、個別でIPアドレスを設定し、この機種のみ同時アクセスを許す方式にしてみた。
    
    結果、ログイン画面までは起動を確認したが、その後、サーバーからの反応がなくなるという状況に陥る。
    そろそろハードコートしているfetchは、環境変数化を検討してもいいのかもしれない。
    なお、スマホで見たら、スマホ向けの見え方になっていない問題を確認。これも要修正。
    今後、スマホを活用しながらフロントエンドを修正してもよいかもしれない。

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // 以下を書き入れることで同じアクセスポイントを共有しているすべての機械と同じローカルホストでの利用を共有する.
+    server: {
+    host: true, // 0.0.0.0で全ネットワークインターフェースからの接続を受け付ける
+  },
 })
